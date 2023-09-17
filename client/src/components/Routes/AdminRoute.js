454da5ef4cb836977c3ab37e2252c9273a2cdeac
Charu Patel <90954539+Sunshadeshine@users.noreply.github.com>
2023-09-17 16:36:57 +0530

    Update AdminRoute.js

--- client/src/components/Routes/AdminRoute.js
+++ client/src/components/Routes/AdminRoute.js
@@ -11,7 +11,7 @@ export default function AdminRoute() {
   useEffect(() => {
     const authCheck = async () => {
       const res = await axios.get(
-        "http://localhost:8080/api/v1/auth/admin-auth"
+        "https://ecommerce-tau-khaki-89.vercel.app/api/v1/auth/admin-auth"
       );
       if (res.data.ok) {
         console.log("admin route ma aagyi h tu ", res.data.ok);
